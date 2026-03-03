daa174d40383f0c8debe40cee60632c49be4122f
Danilo Galete <galetedanilo@gmail.com>
2026-03-02 23:02:51 -0300

    feat: create profile use case tests

--- src/application/use_cases/create_profile.rs
+++ src/application/use_cases/create_profile.rs
@@ -31,12 +31,117 @@ impl<R: ProfileRepository + Send + Sync> CreateProfileUseCase<R> {
             return Err(ProfileError::AlreadyExists(input.id));
         }
 
-        let profile = Profile::new(id, email, None, None, None, None);
+        let profile = Profile::new(id, email);
+
         self.repository
             .save(&profile)
             .await
             .map_err(|e| ProfileError::InvalidData(e.to_string()))?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{Fake, faker::internet::en::FreeEmail};
+    use uuid::Uuid;
+
+    use super::*;
+    use crate::domain::repositories::profile_repo::MockProfileRepository;
+
+    #[tokio::test]
+    async fn when_input_invalid_should_return_invalid_data_error() {
+        let mock_repo = MockProfileRepository::new();
+
+        let use_case = CreateProfileUseCase::new(mock_repo);
+
+        let input = CreateProfileInput {
+            id: "".to_string(),
+            email: "".to_string(),
+        };
+        let result = use_case.execute(input).await;
+
+        assert!(matches!(result, Err(ProfileError::InvalidData(_))));
+    }
+
+    #[tokio::test]
+    async fn when_input_id_invalid_should_return_invalid_data_error() {
+        let mock_repo = MockProfileRepository::new();
+
+        let use_case = CreateProfileUseCase::new(mock_repo);
+
+        let input = CreateProfileInput {
+            id: "".to_string(),
+            email: FreeEmail().fake(),
+        };
+
+        let result = use_case.execute(input).await;
+
+        assert!(matches!(result, Err(ProfileError::InvalidData(_))));
+    }
+
+    #[tokio::test]
+    async fn when_input_email_invalid_should_return_invalid_data_error() {
+        let mock_repo = MockProfileRepository::new();
+
+        let use_case = CreateProfileUseCase::new(mock_repo);
+
+        let input = CreateProfileInput {
+            id: Uuid::now_v7().to_string(),
+            email: "".to_string(),
+        };
+
+        let result = use_case.execute(input).await;
+
+        assert!(matches!(result, Err(ProfileError::InvalidData(_))));
+    }
+
+    #[tokio::test]
+    async fn when_input_exist_profile_should_return_already_exists_error() {
+        let mut mock_repo = MockProfileRepository::new();
+
+        let input = CreateProfileInput {
+            id: Uuid::now_v7().to_string(),
+            email: FreeEmail().fake(),
+        };
+
+        let profile = Profile::new(Id::generate(), Email::try_new(FreeEmail().fake()).unwrap());
+
+        mock_repo
+            .expect_get_profile_by_id()
+            .times(1)
+            .return_const(Ok(Some(profile)));
+
+        let use_case = CreateProfileUseCase::new(mock_repo);
+
+        let result = use_case.execute(input).await;
+
+        assert!(matches!(result, Err(ProfileError::AlreadyExists(_))));
+    }
+
+    #[tokio::test]
+    async fn when_input_valid_and_not_exist_should_return_ok() {
+        let mut mock_repo = MockProfileRepository::new();
+
+        let input = CreateProfileInput {
+            id: Uuid::now_v7().to_string(),
+            email: FreeEmail().fake(),
+        };
+
+        let profile = Profile::new(Id::generate(), Email::try_new(FreeEmail().fake()).unwrap());
+
+        mock_repo
+            .expect_get_profile_by_id()
+            .times(1)
+            .return_const(Ok(None));
+
+        mock_repo.expect_save().times(1).return_const(Ok(()));
+
+        let use_case = CreateProfileUseCase::new(mock_repo);
+
+        let result = use_case.execute(input).await;
+
+        assert!(result.is_ok())
     }
 }
